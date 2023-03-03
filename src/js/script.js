951bb0fbd7cf0bb34b7b0715f295e963f2466618
belie369 <katarzyna.derbich@gmail.com>
2023-03-03 11:15:04 +0100

    Added resultText function (adding result text in H2 and result details text to paragraph); Added caps and needed span.

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,8 @@ const fetchData = () => {
 		.then((response) => response.json())
 		.then((data) => {
 			const totalScore = summaryListFiller(data);
-			averageScore(totalScore, data.length);
+			const score = averageScore(totalScore, data.length);
+			resultText(score);
 		});
 };
 
@@ -34,11 +35,39 @@ const summaryListFiller = (data) => {
 
 const averageScore = (totalScore, elements) => {
 	const scoreRoundSpan = document.querySelector('.component__result-score--round');
-
 	const resultScore = Math.round(totalScore / elements);
-	console.log(resultScore);
 
 	scoreRoundSpan.textContent = resultScore;
+
+	return resultScore;
+};
+
+const resultText = (score) => {
+	const resultTextH2 = document.querySelector('.component__result-text');
+	const resultDetailsP = document.querySelector('.component__result-details');
+
+	switch (true) {
+		case score > 90:
+			resultTextH2.textContent = 'Excellent';
+			resultDetailsP.textContent = 'You scored higher than 90% of the people who have taken these tests.';
+			break;
+		case score >= 70 && score <= 89:
+			resultTextH2.textContent = 'Great';
+			resultDetailsP.textContent = 'You scored higher than 65% of the people who have taken these tests.';
+			break;
+		case score >= 50 && score <= 69:
+			resultTextH2.textContent = 'Good';
+			resultDetailsP.textContent = 'You scored higher than 45% of the people who have taken these tests.';
+			break;
+		case score >= 49 && score <= 29:
+			resultTextH2.textContent = 'Okay';
+			resultDetailsP.textContent = 'You scored higher than 35% of the people who have taken these tests.';
+			break;
+		case score <= 30:
+			resultTextH2.textContent = 'Not Bad';
+			resultDetailsP.textContent = 'You scored higher than 25% of the people who have taken these tests.';
+			break;
+	}
 };
 
 document.addEventListener('DOMContentLoaded', () => {
